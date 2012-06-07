7579a98ad941526f30f37cb8cb288e364ea04c4f
Alex Sharp <ajsharp@gmail.com>
2012-06-07 14:12:54 -0700

    Add sinatra to test gemfile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'rspec'
 require 'rack'
+require 'sinatra/base'
 
 $:.push(File.expand_path(File.dirname(__FILE__)))
 $:.push(File.expand_path(File.dirname(__FILE__)) + '/../lib')
