2515bfabb15e1544a03690363f269fdb879a2bba
Alex Sharp <ajsharp@gmail.com>
2012-06-07 14:15:05 -0700

    Update and simplify specs

--- spec/rack/key_value_logger_spec.rb
+++ spec/rack/key_value_logger_spec.rb
@@ -1,66 +1,62 @@
 require 'spec_helper'
 require 'stringio'
 
+
 describe "logging non success response bodies" do
+  $drain = StringIO.new
+
+  class TestApp < Sinatra::Base
+    use Rack::KeyValueLogger, :log_failure_response_bodies => true, :logger => Logger.new($drain)
+
+    get '/200' do
+      'Success'
+    end
+
+    get '/422' do
+      status 422
+      {'errors' => {'key' => 'val'}}
+    end
+
+    get '/401' do
+      status 401
+      'Unauthorized'
+    end
+
+    get '/400' do
+      status 400
+      'Fail'
+    end
+  end
+
+  def app
+    @app ||= Rack::Builder.app do
+      run TestApp
+    end
+  end
+
   context 'when the proper option is passed in' do
     it "logs the response body for 401's" do
-      drain = StringIO.new
-      app = Rack::Builder.app do
-        use Rack::KeyValueLogger, :log_failure_response_bodies => true, :logger => Logger.new(drain)
-
-        map '/fail' do
-          run lambda { |e| [401, {'Content-Type' => 'text/plain'}, 'Unauthorized'] }
-        end
-      end
-
-      Rack::MockRequest.new(app).get('/fail')
-      drain.rewind
-      drain.read.should =~ /response_body=Unauthorized/
+      Rack::MockRequest.new(app).get('/401')
+      $drain.rewind
+      $drain.read.should =~ /response_body=.*Unauthorized.*/
     end
 
     it "logs the response body for 400's" do
-      drain = StringIO.new
-      app = Rack::Builder.app do
-        use Rack::KeyValueLogger, :log_failure_response_bodies => true, :logger => Logger.new(drain)
-
-        map '/fail' do
-          run lambda { |e| [400, {'Content-Type' => 'text/plain'}, 'Unauthorized'] }
-        end
-      end
-
-      Rack::MockRequest.new(app).get('/fail')
-      drain.rewind
-      drain.read.should =~ /response_body=Unauthorized/
+      Rack::MockRequest.new(app).get('/400')
+      $drain.rewind
+      $drain.read.should =~ /response_body=.*Fail.*/
     end
 
     it "logs the response body for 422's" do
-      drain = StringIO.new
-      app = Rack::Builder.app do
-        use Rack::KeyValueLogger, :log_failure_response_bodies => true, :logger => Logger.new(drain)
-
-        map '/fail' do
-          run lambda { |e| [422, {'Content-Type' => 'text/plain'}, 'Unauthorized'] }
-        end
-      end
-
-      Rack::MockRequest.new(app).get('/fail')
-      drain.rewind
-      drain.read.should =~ /response_body=Unauthorized/
+      res = Rack::MockRequest.new(app).get('/422')
+      $drain.rewind
+      $drain.read.should =~ /response_body=.*errors.*/
     end
 
     it 'does not log the response body for success endpoints' do
-      drain = StringIO.new
-      app = Rack::Builder.app do
-        use Rack::KeyValueLogger, :log_failure_response_bodies => true, :logger => Logger.new(drain)
-
-        map '/' do
-          run lambda { |e| [200, {'Content-Type' => 'text/plain'}, 'Unauthorized'] }
-        end
-      end
-
-      Rack::MockRequest.new(app).get('/success')
-      drain.rewind
-      drain.read.should_not =~ /response_body=Unauthorized/      
+      Rack::MockRequest.new(app).get('/200')
+      $drain.rewind
+      $drain.read.should_not =~ /response_body=Unauthorized/
     end
   end
 end
